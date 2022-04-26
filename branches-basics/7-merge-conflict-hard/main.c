b077d67d092b5031bd0ab163c3b27c23ddf0774c
simplenocs <98728399+simplenocs@users.noreply.github.com>
2022-04-26 22:49:31 +0700

    Add files via upload

---
+++ branches-basics/7-merge-conflict-hard/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define N_ELEMENTS(arr) (sizeof(arr) / sizeof(*(arr)))
+
+int max_element(int* arr, int size)
+{
+    int max = *arr;
+
+    int* it;
+    for (it = arr + 1; it < arr + size; ++it) {
+        if (*it > max) {
+            max = *it;
+        }
+    }
+
+    return max;
+}
+
+void print_array(int* arr, size_t size)
+{
+    printf("Array: ");
+    size_t i;
+    for (i = 0; i < 10; ++i) {
+
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+
+    const int max = max_element(arr, 10);
+
+    print_array(arr, N_ELEMENTS(arr));
+    printf("Max element: %d\n", max);
+
+    return 0;
+}
---
+++ branches-basics/8-rebase-conflict-hard/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define N_ELEMENTS(arr) (sizeof(arr) / sizeof(*(arr)))
+
+int max_element(int* arr, int size)
+{
+    int max = *arr;
+
+    int* it;
+    for (it = arr + 1; it < arr + size; ++it) {
+        if (*it > max) {
+            max = *it;
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
+    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+
+
+    const int max = max_element(arr, N_ELEMENTS(arr));
+
+    printf("Array: ");
+    size_t i;
+    for (i = 0; i < N_ELEMENTS(arr); ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Max element: %d\n", max);
+
+    return 0;
+}
